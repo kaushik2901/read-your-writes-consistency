815f984173eb66636c28c5ed23f821b227abc80a
Kaushik Jadav <kaushikjadav602@gmail.com>
2025-08-31 11:24:48 +0530

    Remove unnecessary items

--- ReadYourWritesConsistency.API/Program.cs
+++ ReadYourWritesConsistency.API/Program.cs
@@ -5,10 +5,8 @@ using ReadYourWritesConsistency.API.Models;
 using ReadYourWritesConsistency.API.Persistence;
 using ReadYourWritesConsistency.API.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateSlimBuilder(args);
 
-builder.Services.AddSwaggerGen();
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<IAppDbContextFactory, AppDbContextFactory>();
 builder.Services.AddScoped<IDbIntentAccessor, DbIntentAccessor>();
@@ -16,16 +14,8 @@ builder.Services.AddScoped<ICurrentUserAccessor, CurrentUserAccessor>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseMiddleware<DbIntentMiddleware>();
-
-app.MapGet("/health", () => Results.Ok(new { status = "ok" }));
 
 app.MapV1Endpoints();
 app.MapV2Endpoints();
