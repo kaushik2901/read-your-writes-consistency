a002e9cee8b20ed26bd376b72cd6b67d1b9ee00e
Kaushik Jadav <kaushikjadav602@gmail.com>
2025-09-06 20:13:51 +0530

    Add redis

---
+++ ReadYourWritesConsistency.API/Caching/RedisConnectionMultiplexerExtension.cs
@@ -0,0 +1,18 @@
+﻿using StackExchange.Redis;
+
+namespace ReadYourWritesConsistency.API.Caching;
+
+public static class RedisConnectionMultiplexerExtension
+{
+    public static IServiceCollection AddRedis(this IServiceCollection services)
+    {
+        services.AddSingleton<IConnectionMultiplexer>(sp =>
+        {
+            var configuration = sp.GetRequiredService<IConfiguration>();
+            var redisConnectionString = configuration.GetConnectionString("Redis") ?? throw new ArgumentException("Redis connection string is not configured");
+            return ConnectionMultiplexer.Connect(redisConnectionString);
+        });
+
+        return services;
+    }
+}
